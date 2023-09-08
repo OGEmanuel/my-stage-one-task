42172b2ffaecf79820f6f1ee95e34f513acf0e8d
OGEmanuel <1blacksanta416@gmail.com>
2023-09-08 11:07:39 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ main.js
@@ -0,0 +1,13 @@
+const day = document.getElementById('day');
+const time = document.getElementById('time');
+
+const currentDate = new Date();
+const today = currentDate.toLocaleString('en-GB', { weekday: 'long' });
+
+day.textContent = today;
+
+const updateTime = () => {
+  const utcTime = new Date().getUTCMilliseconds();
+  time.textContent = utcTime;
+};
+const setUpdatedtime = setInterval(updateTime, 1000);
